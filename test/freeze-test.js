3a9283124a192755d4e0482e16ed6aa5233f8849
Sergey Kryzhanovsky <skryzhanovsky@gmail.com>
2012-11-06 20:19:00 +0200

    use __dirname in PATH.resolve()

--- test/freeze-test.js
+++ test/freeze-test.js
@@ -4,14 +4,14 @@ var FREEZE = require('../lib/freeze'),
     ASSERT = require('assert'),
     BORSCHIK = require('../lib/borschik'),
 
-    testImagePath = './test/test.png';
+    testImagePath = 'test.png';
 
 function loadTestImage(path) {
     return readFile(path || testImagePath);
 }
 
 function readFile(path) {
-    return FS.readFileSync(PATH.resolve(path));
+    return FS.readFileSync(PATH.resolve(__dirname, path));
 }
 
 describe('sha1Base64', function() {
@@ -36,16 +36,16 @@ describe('fixBase64', function() {
 describe('loadConfig', function() {
 
     it('path', function() {
-        ASSERT.equal(FREEZE.path(PATH.resolve('./test/config_load')), '//test/test/');
+        ASSERT.equal(FREEZE.path(PATH.resolve(__dirname, 'config_load')), '//test/test/');
     });
 
     it('freezePath', function() {
-        ASSERT.ok(FREEZE.freezePath(PATH.resolve('./test/config_load')));
+        ASSERT.ok(FREEZE.freezePath(PATH.resolve(__dirname, 'config_load')));
     });
 
     it('freezeDir', function() {
-        ASSERT.equal(FREEZE.freezeDir(PATH.resolve('./test/config_load/file.png')),
-                     FREEZE.freezePath(PATH.resolve('./test/config_load')));
+        ASSERT.equal(FREEZE.freezeDir(PATH.resolve(__dirname, 'config_load/file.png')),
+                     FREEZE.freezePath(PATH.resolve(__dirname, 'config_load')));
     });
 
 });
@@ -77,8 +77,13 @@ describe('isImageUrl', function() {
 });
 
 function testFreeze(tech, inPath, outPath, okPath) {
+    inPath = PATH.resolve(__dirname, inPath);
+    outPath = PATH.resolve(__dirname, outPath);
+    okPath = PATH.resolve(__dirname, okPath);
+
     before(function(done) {
-        BORSCHIK.api({ tech: tech, input: inPath, output: outPath }).then(function() { done() });
+            BORSCHIK.api({ tech: tech, input: inPath, output: outPath
+        }).then(function() { done() });
     });
 
     it('freeze ' + tech + ' ok', function() {
@@ -92,16 +97,16 @@ function testFreeze(tech, inPath, outPath, okPath) {
 
 describe('freeze from .css (-t css)', function() {
     testFreeze('css',
-               './test/freeze_from_css/test.css',
-               './test/freeze_from_css/_test.css',
-               './test/freeze_from_css/ok_css.css');
+               'freeze_from_css/test.css',
+               'freeze_from_css/_test.css',
+               'freeze_from_css/ok_css.css');
 
 });
 
 describe('freeze from .css (-t css)', function() {
     testFreeze('css-fast',
-               './test/freeze_from_css/test.css',
-               './test/freeze_from_css/_test.css',
-               './test/freeze_from_css/ok_cssfast.css');
+               'freeze_from_css/test.css',
+               'freeze_from_css/_test.css',
+               'freeze_from_css/ok_cssfast.css');
 
 });
